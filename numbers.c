e48a510f5c383213d52d3a5cf29f19e7d2163c66
akhil-1906 <39850202+akhil-1906@users.noreply.github.com>
2018-06-25 13:09:00 +0530

    Add files via upload

---
+++ numbers.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int n,first,second,sum,mul;
+printf("Enter two digit no.");
+scanf("%d",&n);
+if(n<10||n>99)
+{
+printf("invalid no. insert two digit no.");
+}
+else{
+first=n/10;
+second=n%10;
+sum=first+second;
+mul=first*second;
+if(sum+mul==n){
+printf("special number");
+}
+else{
+printf("not");
+}
+}
+return 0;
+}
+
